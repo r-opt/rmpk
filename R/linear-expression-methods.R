f40ebc2c2ce9c03894032b072cf3187a2b75edfc
Dirk Schumacher <mail@dirk-schumacher.net>
2021-11-07 17:31:41 +0100

    Document all S4 methods

--- R/linear-expression-methods.R
+++ R/linear-expression-methods.R
@@ -1,23 +1,38 @@
+#' Add a scalar affine function and a numeric
 #' @include variable-methods.R
+#' @param e1 A scalar affine function
+#' @param e2 A scalar numeric
 setMethod("+", signature(e1 = "MOI_scalar_affine_function", e2 = "numeric"), function(e1, e2) {
   slot(e1, "constant", check = FALSE) <- e1@constant + e2
   e1
 })
 
+#' Add a scalar affine function and a numeric
+#' @param e1 A scalar numeric
+#' @param e2 A scalar affine function
 setMethod("+", signature(e1 = "numeric", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   slot(e2, "constant", check = FALSE) <- e2@constant + e1
   e2
 })
 
+#' Add a scalar affine function and a scalar affine term
+#' @param e1 A scalar affine function
+#' @param e2 A scalar affine term
 setMethod("+", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   slot(e1, "terms", check = FALSE) <- c(e1@terms, list(e2))
   e1
 })
 
+#' Add a scalar affine function and a scalar affine term
+#' @param e1 A scalar affine term
+#' @param e2 A scalar affine function
 setMethod("+", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   e2 + e1
 })
 
+#' Add a scalar affine function and a scalar affine function
+#' @param e1 A scalar affine function
+#' @param e2 A scalar affine function
 #' @include helper.R
 setMethod("+", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   slot(e1, "terms", check = FALSE) <- c(e1@terms, e2@terms) # O(n)
@@ -25,28 +40,46 @@ setMethod("+", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_aff
   e1
 })
 
+#' Substract a scalar affine function and a scalar affine function
+#' @param e1 A scalar affine function
+#' @param e2 A scalar affine function
 setMethod("-", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   # TODO: room for optimization here
   e1 + -1 * e2
 })
 
+#' Substract a scalar affine function and a scalar affine term
+#' @param e1 A scalar affine function
+#' @param e2 A scalar affine term
 setMethod("-", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   e1 + -1 * e2
 })
 
+#' Substract a scalar affine function and a scalar affine term
+#' @param e1 A scalar affine term
+#' @param e2 A scalar affine function
 setMethod("-", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   e1 + -1 * e2
 })
 
+#' Substract a scalar affine function and a numeric
+#' @param e1 A scalar affine function
+#' @param e2 A numeric
 setMethod("-", signature(e1 = "MOI_scalar_affine_function", e2 = "numeric"), function(e1, e2) {
   slot(e1, "constant", check = FALSE) <- e1@constant - e2
   e1
 })
 
+#' Substract a scalar affine function and a numeric
+#' @param e1 A numeric
+#' @param e2 A scalar affine function
 setMethod("-", signature(e1 = "numeric", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
 
+#' Multiply a scalar affine function and a numeric
+#' @param e1 A scalar affine function
+#' @param e2 A numeric
 setMethod("*", signature(e1 = "MOI_scalar_affine_function", e2 = "numeric"), function(e1, e2) {
   if (e2 == 0) {
     return(e2)
@@ -58,26 +91,41 @@ setMethod("*", signature(e1 = "MOI_scalar_affine_function", e2 = "numeric"), fun
   e1
 })
 
+#' Multiply a scalar affine function and a numeric
+#' @param e1 A numeric
+#' @param e2 A scalar affine function
 setMethod("*", signature(e1 = "numeric", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   e2 * e1
 })
 
+#' Multiply a scalar affine function and a scalar affine term
+#' @param e1 A scalar affine function
+#' @param e2 A scalar affine term
 setMethod("*", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   Reduce(function(acc, el) {
     acc + e2 * el
   }, e1@terms, init = 0) + e2 * e1@constant
 })
 
+#' Multiply a scalar affine function and a scalar affine term
+#' @param e1 A scalar affine term
+#' @param e2 A scalar affine function
 setMethod("*", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   e2 * e1
 })
 
+#' Multiply two scalar affine functions
+#' @param e1 A scalar affine function
+#' @param e2 A scalar affine function
 setMethod("*", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   Reduce(function(acc, el) {
     acc + e1 * el
   }, e2@terms, init = 0) + e1 * e2@constant
 })
 
+#' Exponentiate a scalar affine function by a numeric
+#' @param e1 A scalar affine function
+#' @param e2 A numeric of value 2, anything else is not supported
 setMethod("^", signature(e1 = "MOI_scalar_affine_function", e2 = "numeric"), function(e1, e2) {
   stopifnot(e2 == 2)
   e1 * e1
--- R/quadratic-expression-methods.R
+++ R/quadratic-expression-methods.R
@@ -1,4 +1,7 @@
-#' @ include variable-class.R
+#' Multiply two scalar affine terms
+#' @param e1 A scalar affine term
+#' @param e2 A scalar affine term
+#' @include variable-class.R
 setMethod("*", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   moi_scalar_quadratic_term(
     variable1 = e1@variable,
@@ -7,11 +10,17 @@ setMethod("*", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_affine_
   )
 })
 
+#' Exponentiate a scalar affine term by 2
+#' @param e1 A scalar affine term
+#' @param e2 A scalar numeric of value 2
 setMethod("^", signature(e1 = "MOI_scalar_affine_term", e2 = "numeric"), function(e1, e2) {
   stopifnot(e2 == 2)
   moi_scalar_quadratic_term(variable1 = e1@variable, variable2 = e1@variable, coefficient = e1@coefficient^2)
 })
 
+#' Multiply a scalar quadratic term and a numeric
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar numeric
 setMethod("*", signature(e1 = "MOI_scalar_quadratic_term", e2 = "numeric"), function(e1, e2) {
   if (e2 == 0) {
     return(0)
@@ -20,10 +29,16 @@ setMethod("*", signature(e1 = "MOI_scalar_quadratic_term", e2 = "numeric"), func
   e1
 })
 
+#' Multiply a scalar quadratic term and a numeric
+#' @param e1 A scalar numeric
+#' @param e2 A scalar quadratic term
 setMethod("*", signature(e1 = "numeric", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   e2 * e1
 })
 
+#' Multiply a scalar quadratic function and a numeric
+#' @param e2 A scalar numeric
+#' @param e1 A scalar quadratic function
 setMethod("*", signature(e1 = "MOI_scalar_quadratic_function", e2 = "numeric"), function(e1, e2) {
   if (e2 == 0) {
     return(e2)
@@ -34,10 +49,16 @@ setMethod("*", signature(e1 = "MOI_scalar_quadratic_function", e2 = "numeric"), 
   e1
 })
 
+#' Multiply a scalar quadratic function and a numeric
+#' @param e1 A scalar numeric
+#' @param e2 A scalar quadratic function
 setMethod("*", signature(e1 = "numeric", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   e2 * e1
 })
 
+#' Add a scalar quadratic function and a numeric
+#' @param e2 A scalar numeric
+#' @param e1 A scalar quadratic function
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "numeric"), function(e1, e2) {
   moi_scalar_quadratic_function(
     constant = e2,
@@ -46,18 +67,30 @@ setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "numeric"), func
   )
 })
 
+#' Add a scalar quadratic term and a numeric
+#' @param e1 A scalar numeric
+#' @param e2 A scalar quadratic term
 setMethod("+", signature(e1 = "numeric", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   e2 + e1
 })
 
+#' Substract a scalar quadratic term and a numeric
+#' @param e2 A scalar numeric
+#' @param e1 A scalar quadratic term
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_term", e2 = "numeric"), function(e1, e2) {
   e1 + (-1 * e2)
 })
 
+#' Substract a scalar quadratic term and a numeric
+#' @param e1 A scalar numeric
+#' @param e2 A scalar quadratic term
 setMethod("-", signature(e1 = "numeric", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
 
+#' Add a scalar quadratic term and a scalar quadratic term
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar quadratic term
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   moi_scalar_quadratic_function(
     quadratic_terms = list(e1, e2),
@@ -66,6 +99,9 @@ setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_quad
   )
 })
 
+#' Add a scalar quadratic term and a scalar affine term
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar affine term
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   moi_scalar_quadratic_function(
     quadratic_terms = list(e1),
@@ -74,37 +110,61 @@ setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_affi
   )
 })
 
+#' Add a scalar quadratic term and a scalar affine term
+#' @param e1 A scalar affine term
+#' @param e2 A scalar quadratic term
 setMethod("+", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   e2 + e1
 })
 
+#' Substract a scalar quadratic term and a scalar affine term
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar affine term
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   e1 + (-1 * e2)
 })
 
+#' Substract a scalar quadratic term and a scalar affine term
+#' @param e1 A scalar affine term
+#' @param e2 A scalar quadratic term
 setMethod("-", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
 
+#' Add a scalar quadratic function and a scalar numeric
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar numeric
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_function", e2 = "numeric"), function(e1, e2) {
   e1@constant <- e1@constant + e2
   e1
 })
 
+#' Add a scalar quadratic function and a scalar numeric
+#' @param e1 A scalar numeric
+#' @param e2 A scalar quadratic function
 setMethod("+", signature(e1 = "numeric", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   e2 + e1
 })
 
+#' Add a scalar quadratic function and a scalar affine term
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar affine term
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   slot(e1, "affine_terms", check = FALSE) <- c(e1@affine_terms, list(e2))
   e1
 })
 
+#' Add a scalar quadratic function and a scalar quadratic term
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar quadratic term
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   slot(e1, "quadratic_terms", check = FALSE) <- c(e1@quadratic_terms, list(e2))
   e1
 })
 
+#' Add a scalar quadratic function and a scalar quadratic function
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar quadratic function
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   slot(e1, "quadratic_terms", check = FALSE) <- c(e1@quadratic_terms, e2@quadratic_terms)
   slot(e1, "affine_terms", check = FALSE) <- c(e1@affine_terms, e2@affine_terms)
@@ -112,68 +172,117 @@ setMethod("+", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_
   e1
 })
 
+#' Add a scalar quadratic term and a scalar quadratic function
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar quadratic function
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   e2 + e1
 })
 
+#' Substract a scalar quadratic term and a scalar quadratic function
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar quadratic term
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   e1 + (-1 * e2)
 })
 
+#' Substract a scalar quadratic term and a scalar quadratic function
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar quadratic function
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
 
+
+#' Substract a scalar quadratic term and a scalar numeric
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar numeric
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_function", e2 = "numeric"), function(e1, e2) {
   e1 + (-1 * e2)
 })
 
+#' Substract a scalar quadratic term and a scalar numeric
+#' @param e1 A scalar numeric
+#' @param e2 A scalar quadratic function
 setMethod("-", signature(e1 = "numeric", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
 
+#' Add a scalar affine term and a scalar quadratic function
+#' @param e1 A scalar affine term
+#' @param e2 A scalar quadrati function
 setMethod("+", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   e2 + e1
 })
 
+#' Substract a scalar affine term and a scalar quadratic function
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar affine term
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_affine_term"), function(e1, e2) {
   e1 + (-1 * e2)
 })
 
+#' Substract a scalar affine term and a scalar quadratic function
+#' @param e1 A scalar affine term
+#' @param e2 A scalar quadratic function
 setMethod("-", signature(e1 = "MOI_scalar_affine_term", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
 
+#' Add a scalar affine function and a scalar quadratic function
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar affine function
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   e1@affine_terms <- c(e1@affine_terms, e2@terms)
   e1@constant <- e1@constant + e2@constant
   e1
 })
 
+#' Add a scalar affine function and a scalar quadratic function
+#' @param e1 A scalar affine function
+#' @param e2 A scalar quadratic function
 setMethod("+", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   e2 + e1
 })
 
+#' Substract a scalar affine function and a scalar quadratic function
+#' @param e1 A scalar quadratic function
+#' @param e2 A scalar affine function
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_function", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   e1 + (-1 * e2)
 })
 
+#' Substract a scalar affine function and a scalar quadratic function
+#' @param e1 A scalar affine function
+#' @param e2 A scalar quadratic function
 setMethod("-", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_quadratic_function"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
 
+#' Add a scalar affine function and a scalar quadratic term
+#' @param e1 A scalar affine function
+#' @param e2 A scalar quadratic term
 setMethod("+", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   (e2 + 0) + e1
 })
 
+#' Add a scalar affine function and a scalar quadratic term
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar affine function
 setMethod("+", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   e2 + e1
 })
 
+#' Substract a scalar affine function and a scalar quadratic term
+#' @param e1 A scalar affine function
+#' @param e2 A scalar quadratic term
 setMethod("-", signature(e1 = "MOI_scalar_affine_function", e2 = "MOI_scalar_quadratic_term"), function(e1, e2) {
   e1 + (-1 * e2)
 })
 
+#' Substract a scalar affine function and a scalar quadratic term
+#' @param e1 A scalar quadratic term
+#' @param e2 A scalar affine function
 setMethod("-", signature(e1 = "MOI_scalar_quadratic_term", e2 = "MOI_scalar_affine_function"), function(e1, e2) {
   (-1 * e2) - (-1 * e1)
 })
